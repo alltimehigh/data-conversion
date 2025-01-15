8291bbdb616fafcc2f87e76b692fdd795e760a41
kancheng <zxdfgcv@gmail.com>
2025-01-15 13:46:20 +0800

    Added

--- init-labelme.py
+++ init-labelme.py
@@ -1,1 +1,26 @@
-print("init-labelme.py")
+from function.flabelme2yolov8 import *
+from function.flabelme2mask2 import *
+
+import os
+
+def create_default_data_dirs(base_path="./outputs/default_data"):
+    dirs = [
+        os.path.join(base_path, "dataset_labelme"),
+        os.path.join(base_path, "dataset_masks"),
+        os.path.join(base_path, "dataset_yolo")
+    ]
+
+    for directory in dirs:
+        os.makedirs(directory, exist_ok=True)
+        print(f"Directory created: {directory}")
+
+def copy_labelme_dataset(src="./datasets/labelme/labelme_dataset", dest="./outputs/default_data/dataset_labelme"):
+    if os.path.exists(src):
+        shutil.copytree(src, dest, dirs_exist_ok=True)
+        print(f"Contents of '{src}' copied to '{dest}'")
+    else:
+        print(f"Source directory '{src}' does not exist.")
+
+if __name__ == "__main__":
+    create_default_data_dirs()
+    copy_labelme_dataset()
--- init-mask.py
+++ init-mask.py
@@ -1,1 +1,23 @@
-print("init-mask.py")
+import os
+
+def create_default_data_dirs(base_path="./outputs/default_data"):
+    dirs = [
+        os.path.join(base_path, "dataset_labelme"),
+        os.path.join(base_path, "dataset_masks"),
+        os.path.join(base_path, "dataset_yolo")
+    ]
+
+    for directory in dirs:
+        os.makedirs(directory, exist_ok=True)
+        print(f"Directory created: {directory}")
+
+def copy_mask_dataset(src="./datasets/mask/mask_dataset", dest="./outputs/default_data/dataset_mask"):
+    if os.path.exists(src):
+        shutil.copytree(src, dest, dirs_exist_ok=True)
+        print(f"Contents of '{src}' copied to '{dest}'")
+    else:
+        print(f"Source directory '{src}' does not exist.")
+
+if __name__ == "__main__":
+    create_default_data_dirs()
+    copy_mask_dataset()
